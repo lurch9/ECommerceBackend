1629e0c9b7b668af86b9c1452ebed7a3683289fd
lurch <alexhann17@yahoo.com>
2022-01-23 16:32:24 -0800

    added models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,13 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category); // Products belongsTo Category
 
-// Categories have many Products
+Category.hasMany(Product);// Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag }); // Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag }); // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
   Product,
